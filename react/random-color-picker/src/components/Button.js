f18ce6342dd32fcc93b8c29ff4b3aa8ba7804596
Arman <persakiss@gmail.com>
2022-06-13 12:22:20 +0400

    random color picker, this state

--- react/random-color-picker/src/components/Button.js
+++ react/random-color-picker/src/components/Button.js
@@ -4,7 +4,6 @@ export class Button extends React.Component {
 	render() {
 		return (
 			<button 
-    {/* Now that you’ve defined an event handler, you can pass it to another component as a prop. This is a pattern that you’ll see much more of in the next course. In Random‘s render method, give <Button /> an attribute with a name of onClick. Set onClick‘s value equal to the handleClick method. */}
         onClick={ this.props.onClick }
 				className={ this.props.light ? 'light-button' : 'dark-button' }>
 				Refresh
