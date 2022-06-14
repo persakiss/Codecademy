075fa55ef75cd51a334df8f436be1b11e3f82928
Arman <persakiss@gmail.com>
2022-06-14 11:35:57 +0400

    parent child sibling states, and started video player project

---
+++ react/video-player/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Video } from './components/Video';
+import { Menu } from './components/Menu';
+
+const VIDEOS = {
+  fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
+  slow: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
+  cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
+  eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4'
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { src: VIDEOS.fast };
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Video Player</h1>
+        <Menu />
+        <Video />
+      </div>
+    );
+  }
+}
+
+
+export default App;
